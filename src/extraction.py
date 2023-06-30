c948784b4944397c64dbb59155c8e620ba97bae2
Lucianom101 <mauricioluciano23@gmail.com>
2023-06-29 21:31:24 -0300

    Finalizado o projeto

--- src/extraction.py
+++ src/extraction.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
+
 def load_data():
-    return pd.read_csv('Inicialização/3_Git_para_DS/7_Projeto/data/processed/bikes_completed.csv')
+    return pd.read_csv("data/bikes_completed.csv")
